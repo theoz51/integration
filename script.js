d319bf70638cd13b03e7e639cd31d7f0956987eb
Theo <theo.zubac@gmail.com>
2022-05-20 08:44:58 +0200

    fini

---
+++ script.js
@@ -0,0 +1,9 @@
+var slidermain = document.getElementById("slider");
+var item = slidermain.getElementsByClassName("item")
+function suivant(){
+    slidermain.append(item[0]);
+    
+}
+function arriere(){
+    slidermain.prepend(item[item.length - 1]);
+}
